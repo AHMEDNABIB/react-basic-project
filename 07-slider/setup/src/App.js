6fc6cd082f1117c045180684e61a6a4d401acd10
AHMED NABIB <nabibpallab@gmail.com>
2023-04-10 02:03:33 +0600

    slider compete

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -4,43 +4,107 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState([])
+  const [index, setIndex] = useState(0)
+
+  const nextSlide = () => {
+		setIndex((oldIndex) => {
+			let index = oldIndex + 1;
+			if (index > people.length - 1) {
+				index = 0;
+			}
+			return index;
+		});
+  };
+  const prevSlide = () => {
+		setIndex((oldIndex) => {
+			let index = oldIndex - 1;
+			if (index < 0) {
+				index = people.length - 1;
+			}
+			return index;
+		});
+  };
+
+  // Using useEffect
+  //  useEffect(() => {
+	// 	const lastIndex = people.length - 1;
+	// 	if (index < 0) {
+	// 		setIndex(lastIndex);
+	// 	}
+	// 	if (index > lastIndex) {
+	// 		setIndex(0);
+	// 	}
+  //  }, [index, people]);
+  
+    useEffect(() => {
+		let slider = setInterval(() => {
+			  setIndex((oldIndex) => {
+					let index = oldIndex + 1;
+					if (index > people.length - 1) {
+						index = 0;
+					}
+					return index;
+				});
+    }, 5000);
+      // console.log(slider);
+		return () => {
+      clearInterval(slider);
+      // console.log(slider);
+		};
+	}, [index]);
+
   return (
 		<section className="section">
 			<div className="title">
 				<h2>
-					<span></span> reviews
+					<span>/</span>reviews
 				</h2>
 			</div>
-
 			<div className="section-center">
-				{people.map((person, index) => {
+				{people.map((person, personIndex) => {
 					const { id, image, name, title, quote } = person;
 
+					// console.log(personIndex, index);
+
+					let position = "nextSlide";
+					if (personIndex === index) {
+						position = "activeSlide";
+					}
+					if (
+						personIndex === index - 1 ||
+						(index === 0 && personIndex === people.length - 1)
+					) {
+						position = "lastSlide";
+					}
+
 					return (
-						<article key={id}>
+						<article className={position} key={id}>
 							<img
 								src={image}
 								alt={name}
 								className="person-img"
 							/>
-
 							<h4>{name}</h4>
-
 							<p className="title">{title}</p>
-							<p className="quote">{quote}</p>
+							<p className="text">{quote}</p>
 							<FaQuoteRight className="icon" />
 						</article>
 					);
 				})}
-			</div>
-			<button className="prev">
-				<FiChevronLeft />
-			</button>
+				{/* <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button> */}
 
-			<button className="next">
-				<FiChevronRight />
-			</button>
+				<button className="prev" onClick={prevSlide}>
+					<FiChevronLeft />
+				</button>
+				<button className="next" onClick={nextSlide}>
+					<FiChevronRight />
+				</button>
+			</div>
 		</section>
   );
 }
