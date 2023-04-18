f7cec840f71c683584a54ff323321eff002303aa
AHMED NABIB <nabibpallab@gmail.com>
2023-04-18 15:25:05 +0600

    stripe submenu complete

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,60 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({})
+  const [page, setPage] = useState({ page: "", links: [] });
+
+	const openSidebar = () => {
+		setIsSidebarOpen(true);
+	};
+	const closeSidebar = () => {
+		setIsSidebarOpen(false);
+    };
+    
+    // console.log(sublinks)
+
+    const openSubmenu = (text, coordinates) => {
+
+        const page = sublinks.find((link) => {
+            return link.page === text; 
+            // return console.log(link.page === text); 
+        });
+		setPage(page);
+        //  console.log(page)
+       
+        setLocation(coordinates);
+		setIsSubmenuOpen(true);
+	};
+	const closeSubmenu = () => {
+		setIsSubmenuOpen(false);
+	};
+
+	return (
+		<AppContext.Provider
+			value={{
+				isSidebarOpen,
+				openSidebar,
+				closeSidebar,
+				isSubmenuOpen,
+				openSubmenu,
+                closeSubmenu,
+                location,
+                page
+			}}>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
