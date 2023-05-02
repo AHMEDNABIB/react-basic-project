32bba3d5e48117675766f57275727161f7d0d9e6
AHMED NABIB <nabibpallab@gmail.com>
2023-05-03 02:32:26 +0600

    cocktails project complete

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis dolor reiciendis alias molestiae, deserunt laborum culpa? Nisi odio qui voluptas nobis sint numquam, quidem obcaecati, at amet accusamus, eum similique? Expedita unde dolorem velit quae at reiciendis dolore distinctio adipisci in earum excepturi molestiae aspernatur id nisi, sit hic quaerat quisquam numquam ratione! Eum placeat sunt eaque praesentium odio omnis vitae similique beatae suscipit ducimus voluptate nam doloremque doloribus, excepturi non temporibus alias eveniet? Temporibus mollitia explicabo expedita! Pariatur, debitis! Ab aperiam nostrum totam. Tenetur quam in nostrum id eius impedit labore officia perspiciatis. Modi nam impedit aut debitis odit.
+      </p>
+    </section>
   )
 }
 
